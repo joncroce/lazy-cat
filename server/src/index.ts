a76d507f1a6c4fe282d5b5e0c2b0724068586573
Jon Croce <crocedev@gmail.com>
2023-01-24 07:30:05 -0500

    add logging

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,18 @@
 import { config } from 'dotenv';
 import express from 'express';
+import morgan from 'morgan';
 import mountRoutes from './routes';
 
 config();
 
 const port = process.env.SERVER_PORT;
 const app = express();
+app.use(morgan('tiny'));
 
 mountRoutes(app);
 
 app.use(express.json());
 
 app.listen(port, () => {
-	console.log(`Listening on port ${port}`)
+	console.log(`Listening on port ${port}`);
 });
