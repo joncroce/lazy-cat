59d85ea9e769cffa0d9ddd0de3a02057d3ff32cd
Jon Croce <crocedev@gmail.com>
2023-01-24 07:04:51 -0500

    create users controller

---
+++ server/src/controllers/users.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from "express";
+import db from '../db';
+
+const getById = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { rows } = await db.query('SELECT * FROM users WHERE id = $1', [id]);
+  res.json(rows[0]);
+};
+
+export default {
+	getById
+}
--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -1,12 +1,8 @@
 import Router from 'express-promise-router';
-import db from '../db';
+import Users from "../controllers/users";
 
 const router = Router();
 
-router.get('/:id', async (req, res) => {
-  const { id } = req.params;
-  const { rows } = await db.query('SELECT * FROM users WHERE id = $1', [id]);
-  res.json(rows[0]);
-});
+router.get('/:id', Users.getById);
 
 export default router;
