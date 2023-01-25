55057864880f165fb282ab8b78ac50c60200704d
Jon Croce <crocedev@gmail.com>
2023-01-25 00:46:14 -0500

    add check for existing cat in update validator

--- server/src/cats/validators.ts
+++ server/src/cats/validators.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 import { body, validationResult, matchedData, ValidationChain } from 'express-validator';
+import { ShowCatService } from './services';
 
 const runValidations = async (validations: ValidationChain[], req: Request) =>
 	Promise.all(validations.map(validation => validation.run(req)));
@@ -18,13 +19,25 @@ export const StoreCatValidator = async (req: Request, res: Response, next: NextF
 
 	req.body = matchedData(req, { locations: ['body'] });
 	if (Object.keys(req.body).length === 0) {
-		return res.status(400).json("No valid fields submitted for storing cat.");
+		return res.status(400).json("Storing failed; no valid fields submitted.");
 	}
 
 	next();
 };
 
 export const UpdateCatValidator = async (req: Request, res: Response, next: NextFunction) => {
+	const id = +req.params.id;
+	if (isNaN(id) || id <= 0) {
+		return res.status(400).json("Update failed; invalid ID given.");
+	}
+
+	try {
+		await ShowCatService(id);
+	} catch (e: unknown) {
+		console.error(e);
+		return res.status(400).json(`Update failed; no cat found with that ID.`);
+	}
+
 	const validations = [
 		body('name').optional().isString().trim().escape().isLength({ min: 1, max: 50 }),
 		body('description').optional().isString().trim().escape().isLength({ min: 0, max: 500 })
@@ -38,12 +51,7 @@ export const UpdateCatValidator = async (req: Request, res: Response, next: Next
 
 	req.body = matchedData(req, { locations: ['body'] });
 	if (Object.keys(req.body).length === 0) {
-		return res.status(400).json("No valid fields submitted for updating cat.");
-	}
-
-	const id = +req.params.id;
-	if (isNaN(id) || id <= 0) {
-		return res.status(400).json("Invalid ID");
+		return res.status(400).json("Update failed; no valid fields submitted.");
 	}
 
 	next();
